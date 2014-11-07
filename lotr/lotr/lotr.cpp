0d331ada266e8584ed4ed84bdcd484396c8b0f98
Sp0x <sp0x@xakep.ru>
2014-11-07 08:13:53 +0200

    Updated sketch of second class

---
+++ lotr/lotr/lotr.cpp
@@ -0,0 +1,60 @@
+#include <cstring>
+
+#define Three int
+#define Rings main() {int the=3;
+#define Elven 2
+#define kings 1
+#define under ;
+#define sky < 24
+#define Seven the+=7
+#define Dwarf 2
+#define lords 1
+#define in ;
+#define their the
+#define halls <
+#define of
+#define stone 17
+#define Nine the+=9
+#define Mortal 1
+#define men ;
+#define doomed the
+#define to <
+#define die 7
+#define One the+=1
+#define Dark 1
+#define Lord ;
+#define on the
+#define his !=
+#define dark 7
+#define throne ;
+
+#define In if
+#define Land ==
+#define Mordor 24
+#define where
+#define Shadows -=strlen
+
+#define Ring ;if (the
+#define rule 22
+#define all 0
+#define them );
+#define find 23
+#define bring 24
+#define and ;
+#define darkness +=
+#define bind 1
+
+#define CopyRight printf
+#define c "\n%d",the
+#define by ;
+#define Mordred }
+
+Three Rings for (the += Elven - kings under the sky;
+Seven) for (the += Dwarf - lords in their halls of stone;
+Nine) for (the += Mortal men doomed to die;
+One) for (the += Dark Lord on his dark throne);
+In(the Land of Mordor) where the Shadows("lie.");
+One Ring to rule them all, One Ring to find them
+One Ring to bring them all and In(the darkness bind them
+In(the Land of Mordor) where the Shadows("lie.");
+}
