dd878187b147222a0654e9d06462431a2a4337de
Sp0x <sp0x@xakep.ru>
2014-11-07 08:25:30 +0200

    Compile check

--- fmi_up/fmi_up/op1.cpp
+++ fmi_up/fmi_up/op1.cpp
@@ -1,92 +1,95 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+#include <string>
 
 using namespace std;
-
-
-
+template<class T>
+using alias = T;
 void main(){
 	cout << "Hi, FMI";
 }
+class Op1{
+public:
 
-
-void zad2(){
-	int age = 59;
-	cout << "Hi I'm " << age << " and my name\s Gates Bill";
-}
-
-void zad3(){
-	bool lovePrograming = true;
-	bool loveLudogorets = false;
-	bool programShouldHAVEComments = true;
-	cout << "I love programming" << lovePrograming;
-	cout << "I love Ludogorets" << loveLudogorets;
-	cout << "Programs should have comments " << programShouldHAVEComments;
-}
-
-
-/**
- Prints out of our 5 friends.
-*/
-void zad4(){
-	//Array with friends
-	string friends[4] = { "friend0", "friend1", "friend2", "friend3", "friend4" };
-	for (int i = 0; i <= 4; i++){
-		cout << friends[i].c_str;
+	void zad2(){
+		int age = 59;
+		cout << "Hi I'm " << age << " and my name\s Gates Bill";
 	}
-}
 
-int zad5(){
-	int favoriteNumber;
-	cout << "What is your favorite number?" << endl;
-	cin >> favoriteNumber;
-	cout << "Oh, " << favoriteNumber << " is my favorite number, too!" << endl;
-	return favoriteNumber;
-}
-
-void zad6(){
-	float sides[3] = {0.0f, 0.0f, 0.0f};
-	float p;
-	cout << "Parameter calculator";
-	for (int iSide = 0; iSide < 3; iSide++)
-	{
-		cout << "Enter side " << iSide << ": "; cin >> sides[iSide];
-		p += sides[iSide];
+	void zad3(){
+		bool lovePrograming = true;
+		bool loveLudogorets = false;
+		bool programShouldHAVEComments = true;
+		cout << "I love programming" << lovePrograming;
+		cout << "I love Ludogorets" << loveLudogorets;
+		cout << "Programs should have comments " << programShouldHAVEComments;
 	}
-	cout << "Perimeter " << p;
-}
 
-void zad7(){
-	float singlePrice;
-	int count = 0;
-	cout << "Enter the price of the thing you're buying";
-	cin >> singlePrice;
-	cout << "How many items are you going to buy ?";
-	cin >> count;
-	cout << "The Bulgarian VAT tax for this purchase is: " <<
-		0.20*(singlePrice*count);
-}
 
-void zad8(){
-	float a[1], b[1];
-	cout << "Enter the coordinates of point A";
-	cin >> a[0]; cin >> a[1];
-	cout << "Enter the coordinates of point B";
-	cin >> b[0]; cin >> b[1];
-	float dist = sqrt( pow((b[0] - a[0]),2) + pow((b[1] - a[1]),2));
-	cout << "Distance is " << dist;
-}
+	/**
+	Prints out of our 5 friends.
+	*/
+	void zad4(){
+		//Array with friends
+		std::string* friends = alias<std::string[]>{ "friend0", "friend1", "friend2", "friend3", "friend4" };
+		for (int i = 0; i <= 4; i++){
+			cout << (friends[i]).c_str();
+		}
+	}
 
-void zad9(){
-	int myAge = 0;
-	int currentYear = 2014;
-	int compareYear;
+	int zad5(){
+		int favoriteNumber;
+		cout << "What is your favorite number?" << endl;
+		cin >> favoriteNumber;
+		cout << "Oh, " << favoriteNumber << " is my favorite number, too!" << endl;
+		return favoriteNumber;
+	}
 
-	cout << "Your age: ";  cin >> myAge;
-	cout << "Current year: ";  cin >> currentYear;
-	cout << "Enter the year to compare with: ";  cin >> compareYear;
+	void zad6(){
+		float sides[3] = { 0.0f, 0.0f, 0.0f };
+		float p = 0.0f;
+		cout << "Parameter calculator";
+		for (int iSide = 0; iSide < 3; iSide++)
+		{
+			cout << "Enter side " << iSide << ": "; cin >> sides[iSide];
+			p += sides[iSide];
+		}
+		cout << "Perimeter " << p;
+	}
 
-	if (abs(compareYear - currentYear) > (150-myAge))
-		cout << "It's been 150 years, you should be dead by then :D";
-}
+	void zad7(){
+		float singlePrice;
+		int count = 0;
+		cout << "Enter the price of the thing you're buying";
+		cin >> singlePrice;
+		cout << "How many items are you going to buy ?";
+		cin >> count;
+		cout << "The Bulgarian VAT tax for this purchase is: " <<
+			0.20*(singlePrice*count);
+	}
+
+	void zad8(){
+		float a[1], b[1];
+		cout << "Enter the coordinates of point A";
+		cin >> a[0]; cin >> a[1];
+		cout << "Enter the coordinates of point B";
+		cin >> b[0]; cin >> b[1];
+		float dist = sqrt(pow((b[0] - a[0]), 2) + pow((b[1] - a[1]), 2));
+		cout << "Distance is " << dist;
+	}
+
+	void zad9(){
+		int myAge = 0;
+		int currentYear = 2014;
+		int compareYear;
+
+		cout << "Your age: ";  cin >> myAge;
+		cout << "Current year: ";  cin >> currentYear;
+		cout << "Enter the year to compare with: ";  cin >> compareYear;
+
+		if (abs(compareYear - currentYear) > (150 - myAge))
+			cout << "It's been 150 years, you should be dead by then :D";
+	}
+
+};
--- fmi_up/fmi_up/op2.cpp
+++ fmi_up/fmi_up/op2.cpp
@@ -5,151 +5,158 @@
 
 using namespace std;
 bool isPrime (int num);
-template <typex>
-int* toArray(stack<typex> stk, int & size);
+template <typename TStackType>
+int* toArray(stack<TStackType> stk, int & size);
 char * toBinary(int num);
-
-void zad1(){
-    char c;
-	cin >> c;
-    string result;
-    if(isdigit(c)){
-		result = "number";
-    }else{
-        result = isupper(c) ? "Upper-case letter" : "Lower-case letter";
-    }
-    cout << result.c_str();
-}
-
-
-/**
-* Check if input was a isPrime number
-*/
-void zad2(){
-    int number;
-    cin >> number;
-    cout << isPrime(number) ? "prime" : "not prime";
-}
-bool isPrime (int num)
-{
-    if (num <=1)
-        return false;
-    else if (num == 2)
-        return true;
-    else if (num % 2 == 0)
-        return false;
-    else
-    {
-        bool isPrime = true;
-        int divisor = 3;
-        double num_d = static_cast<double>(num);
-        int upperLimit = static_cast<int>(sqrt(num_d) +1);
-
-        while (divisor <= upperLimit)
-        {
-            if (num % divisor == 0)
-                isPrime = false;
-            divisor +=2;
-        }
-        return isPrime;
-    }
-}
-
-
-
-stack<int> zad3(int number){
-    if(number==0) number=1234;
-    std::stack<int> stk;
-    do{
-        int rem = number % 10;
-        number = number /10;
-		stk.push(rem);
-	} while (number > 1);
-	while (stk.size > 0){
-		cout << (int)stk.top() << "\n"; stk.pop();
+class Op2{
+public:
+	void zad1(){
+		char c;
+		cin >> c;
+		string result;
+		if (isdigit(c)){
+			result = "number";
+		}
+		else{
+			result = isupper(c) ? "Upper-case letter" : "Lower-case letter";
+		}
+		cout << result.c_str();
 	}
-}
-
-void zad4(){
-    cout << "1-  One digit number\n";
-    cout << "2-  Two digits numbers\n";
-    cout << "3-  Three digits number\n";
-    cout << "4-  Four digits number\n";
-    cout << "5-  Five digits number\n";
-    int numDigits; cin >> numDigits;
-    cout << "Enter your number: ";
-    int num; cin >> num;
-    zad3(num);
-}
-/**
-* Checks for palindroms
-**/
-void zad5(){
-    int palindrom; cin >> palindrom;
-    int sz=0;
-    int * pNums= toArray<int>( zad3(palindrom) ,sz);
-    bool isInvalid = false;
-    if(sz%2!=0){
-        cout<<"Not a palindrome\n"; return;
-    }
-    for(int i=0; i<sz; i++){
-        if( pNums[i] != pNums[sz-i]){
-            isInvalid=true;
-        }
-    }
-    cout << palindrom << " Is " << (isInvalid ? "not" : "a") <<  " palindrome!";
-}
-
-void zad6(){
-    int num; cin >> num;
-    cout << toBinary(num);
-}
 
 
-void zad7(){
-    cout << "Number of contendors: ";
-    int nContendors; cin >> nContendors;
-
-
-}
-
-void zad8(){
-    int n=4;
-	int tmpI = 0;
-    char c= '*';
-    for(int i=0; i < n; i++) cout << c << " ";
-    for(int i=n-1; i>0; i--){
-        for(int j =0; j < n; j++){
-			cout << ((j == (i + 1) || j == ((n * 2 - 1) - i)) ? c : ' ');
-//       + + + + +    
-//        +     +
-//         +   +
-//          + +
-//           +
-        }
-    }
-}
-
-char * toBinary(int num){
-	char buff[8];
-    if(num>256){
-        cout << "NOT IMPLEMENTED";
-    }else{
-        for(int i = 7; i > -1; i-- ){
-			buff[i] = num % (int)pow(2, i) == 0;
-            num = num / pow(2,i);
-        }
-    }
-    return buff;
-}
-
-template <typex>
-int* toArray(stack<typex> stk, int & size){
-	size = stk.size();
-	int * ret = new int[size];
-	int pos = 0;
-	while (stk.size() > 0){
-		ret[pos++] = stk.top(); stk.pop();
+	/**
+	* Check if input was a isPrime number
+	*/
+	void zad2(){
+		int number;
+		cin >> number;
+		cout << isPrime(number) ? "prime" : "not prime";
 	}
-	return ret;
-}
+	bool isPrime(int num)
+	{
+		if (num <= 1)
+			return false;
+		else if (num == 2)
+			return true;
+		else if (num % 2 == 0)
+			return false;
+		else
+		{
+			bool isPrime = true;
+			int divisor = 3;
+			double num_d = static_cast<double>(num);
+			int upperLimit = static_cast<int>(sqrt(num_d) + 1);
+
+			while (divisor <= upperLimit)
+			{
+				if (num % divisor == 0)
+					isPrime = false;
+				divisor += 2;
+			}
+			return isPrime;
+		}
+	}
+
+
+
+	stack<int> zad3(int number){
+		if (number == 0) number = 1234;
+		std::stack<int> stk;
+		std::stack<int> stkCopy;
+		do{
+			int rem = number % 10;
+			number = number / 10;
+			stk.push(rem);
+		} while (number > 1);
+		stkCopy = stk;
+		while (stk.size() > 0){
+			cout << (int)stk.top() << "\n"; stk.pop();
+		}
+		return stkCopy;
+	}
+
+	void zad4(){
+		cout << "1-  One digit number\n";
+		cout << "2-  Two digits numbers\n";
+		cout << "3-  Three digits number\n";
+		cout << "4-  Four digits number\n";
+		cout << "5-  Five digits number\n";
+		int numDigits; cin >> numDigits;
+		cout << "Enter your number: ";
+		int num; cin >> num;
+		zad3(num);
+	}
+	/**
+	* Checks for palindroms
+	**/
+	void zad5(){
+		int palindrom; cin >> palindrom;
+		int sz = 0;
+		int * pNums = toArray<int>(zad3(palindrom), sz);
+		bool isInvalid = false;
+		if (sz % 2 != 0){
+			cout << "Not a palindrome\n"; return;
+		}
+		for (int i = 0; i<sz; i++){
+			if (pNums[i] != pNums[sz - i]){
+				isInvalid = true;
+			}
+		}
+		cout << palindrom << " Is " << (isInvalid ? "not" : "a") << " palindrome!";
+	}
+
+	void zad6(){
+		int num; cin >> num;
+		cout << toBinary(num);
+	}
+
+
+	void zad7(){
+		cout << "Number of contendors: ";
+		int nContendors; cin >> nContendors;
+
+
+	}
+
+	void zad8(){
+		int n; cout << "Enter N: "; cin >> n;
+		int tmpI = 0;
+		char c; cout << "Enter triangle char: "; cin >> c;
+		for (int i = 0; i < n; i++) cout << c << " ";
+		for (int i = n - 1; i>0; i--){
+			for (int j = 0; j < n; j++){
+				cout << ((j == (i + 1) || j == ((n * 2 - 1) - i)) ? c : ' ');
+				//       + + + + +    
+				//        +     +
+				//         +   +
+				//          + +
+				//           +
+			}
+		}
+	}
+
+	char * toBinary(int num){
+		char buff[8];
+		if (num>256){
+			cout << "NOT IMPLEMENTED";
+		}
+		else{
+			for (int i = 7; i > -1; i--){
+				buff[i] = num % (int)pow(2, i) == 0;
+				num = num / pow(2, i);
+			}
+		}
+		return buff;
+	}
+
+	template <typename TStackType>
+	int* toArray(stack<TStackType> stk, int & size){
+		size = stk.size();
+		int * ret = new int[size];
+		int pos = 0;
+		while (stk.size() > 0){
+			ret[pos++] = stk.top(); stk.pop();
+		}
+		return ret;
+	}
+};
